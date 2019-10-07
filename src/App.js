1946cec48ac0dea65cdafeaacd1665521b953fd7
Elad <mastik8@gmail.com>
2019-10-07 10:58:21 +0300

    react bootsrap + routes and pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
 
 class App extends React.Component {
-  render(){
-  return (
-    <div>
-      bla bla
-    </div>
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/signup">
+          <SignupPage />
+        </Route>
+      </Switch>
   );
-}
+  }
 }
 
 export default App;
---
+++ src/pages/SignupPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class SignupPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    render() { 
+        return (  
+            <h1>Signup</h1>
+        );
+    }
+}
+
+export default SignupPage;
