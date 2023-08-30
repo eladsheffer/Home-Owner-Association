05d9d3bba75fa2f9a28c3e1d5bc48e0ab7fe43e3
mastik8 <mastik8@gmail.com>
2023-08-30 16:55:17 +0300

    Updates

---
+++ precache-manifest.b8a59a949ee4846ae58aeeefbd1ef1a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73a91aecdcf20fe46ecbdd33f686fd3",
+    "url": "/home-owner-association/index.html"
+  },
+  {
+    "revision": "5cdb92ffbd365e623dc0",
+    "url": "/home-owner-association/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "93942a9681ac99dbcb3b",
+    "url": "/home-owner-association/static/css/main.c5d3d8c7.chunk.css"
+  },
+  {
+    "revision": "5cdb92ffbd365e623dc0",
+    "url": "/home-owner-association/static/js/2.10c2954e.chunk.js"
+  },
+  {
+    "revision": "93942a9681ac99dbcb3b",
+    "url": "/home-owner-association/static/js/main.d3c3fa3a.chunk.js"
+  },
+  {
+    "revision": "6778d72478b46cf37322",
+    "url": "/home-owner-association/static/js/runtime-main.1d0fb1df.js"
+  }
+]);
